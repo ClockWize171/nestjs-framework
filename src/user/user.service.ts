218786d2c29c78edb2d5e5d589a1bd22f2adda07
ClockWize171 <jupiterminhtin998@gmail.com>
2023-04-25 21:44:48 +0630

    mysql connection

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,14 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entity/user.entity';
 
 @Injectable()
 export class UserService {
-  get() {
-    return {
-      user: 'clockwize_thet min htin',
-      email: 'clockwize@gmail.com',
-    };
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  get(): Promise<User[]> {
+    return this.userRepository.find();
+    // return {
+    //   user: 'clockwize_thet min htin',
+    //   email: 'clockwize@gmail.com',
+    // };
   }
 
   create(createUserDto: CreateUserDto) {
